6e0f242d298e37715aeb3d3e21d38e172f40200b
Ruhan dos Reis <ruhandosreis@Ruhans-iMac.local>
2017-11-14 21:41:08 -0200

    install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,18 @@
 sudo true
 
 # Install docker
-wget -qO- https://get.docker.com/ | sh
+if which docker > /dev/null ; then
+    echo "Docker is already installed"
+ else
+ 	wget -qO- https://get.docker.com/ | sh	
+fi
 
 # Install docker-compose
-sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.15.0/docker-compose-$(uname -s)-$(uname -m)"
-sudo chmod +x /usr/local/bin/docker-compose
+if which docker-compose > /dev/null ; then
+	echo "Docker Compose is already installed"
+else
+	sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.15.0/docker-compose-$(uname -s)-$(uname -m)"
+	sudo chmod +x /usr/local/bin/docker-compose
+fi
 
 sudo docker-compose build --no-cache
